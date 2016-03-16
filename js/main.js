4c6742a6c0ad47c1db8268dd4d8f825248e7e246
Howe <hhhaohao@163.com>
2016-03-17 00:41:04 +0800

    add all icon

--- js/main.js
+++ js/main.js
@@ -7,7 +7,24 @@
         "lieyan|烈焰|zhi|大炎爆术,火焰神石,烈焰风暴,魔法学识",
         "yingci|影刺|min|幻影突袭,撕裂月刃,暗影突袭,极限强袭",
         "jiansheng|剑圣|min|旋风式,双意式,逆刃式,心剑式",
-        "xingjiefashi|星界法师|zhi|法师护甲,奥术光辉,法师之怒,奥法之躯"
+        "xingjiefashi|星界法师|zhi|法师护甲,奥术光辉,法师之怒,奥法之躯",
+        "fengxingyouxi|风行游侠|min|强击风箭,游侠标记,疾风速射,风行天赋",
+        "xieshou|邪狩|min|恶魔附体,极限闪避,护身邪影,邪恶狩猎",
+        "xiaoheilong|小黑龙|li|黑龙形态,火焰吐息,龙焰重击,黑龙血统",
+        "manhuang|蛮荒|li|敲一棒,吃一口,吼一声,秀一次",
+        "deluyi|德鲁伊|zhi|召唤树人,纠缠之种,枭兽形态,先知祝福",
+        "fengshenxunshou|月神巡守|min|月光陨落,小小虎啸,冲击之箭,月神巡守",
+        "shuangyuyouxia|霜语游侠|min|霜语之舞,寒霜冰箭,范围沉默,强击光环",
+        "bingzhishengnv|冰之圣女|zhi|暴风之雪,冰雪光环,深度冻结,冰霜新星",
+        "tutengjisi|图腾祭祀|min|群蛇召唤,变形妖术,束缚枷锁,治疗图腾",
+        "senlinzhinv|森林之女|zhi|精灵之助,星火投射,森林祝福,无邪魅诱",
+        "modusha|墨杜莎|min|石化凝视,小分裂箭,能量抽取,潮汐加持",
+        "huimieshushi|毁灭术士|zhi|混乱之石,恶魔咒印,灵魂链接,群暗影箭",
+        "xiaobainiu|小白牛|li|守护嘲讽,极限追击,无限冲锋,守护信仰",
+        "shenshengqishi|神圣骑士|li|神圣震击,圣洁护盾,防御圣盾,献祭圣焰",
+        "shiwang|狮王|li|满怒斩杀,反击重剑,野蛮横扫,嗜血破甲",
+        "yingwuyouxia|影舞游侠|min|纷红风刃,作战激活,致命投掷,影舞天赋",
+        "fengbosaman|风暴萨满|zhi|大闪电链,闪电护盾,元素之握,连击图腾"
     ];
     var swiper = new Swiper('.main_swiper', {
         direction: 'vertical',
@@ -43,7 +60,7 @@
         var curWidth = cur.width * 107/95;
         $('.curhero').remove();
         $('.hero_' + value).append('<img class="curhero" src="img/hero_cur.png" width="' + curWidth + 'px" alt="">');
-        $('#hero_big').html('<img src="img/hero_' + value + '_full.png" alt="">');
+        $('#hero_big').html('<img src="img/hero/hero_' + value + '_full.png" alt="">');
 
         var curHero = heroObj[value].split('|');
         $('.hero_name').html(curHero[1]);
@@ -63,7 +80,7 @@
         var init_big_img = '';
         for(var i = 0; i < hero.length; i++){
             var curHero = hero[i].split('|');
-            html += '<div class="swiper-slide hero_'+ curHero[0] +'" value="' + curHero[0] + '"><img class="hero_swipe_img" src="img/hero_' + curHero[0] +'.png"></div>';
+            html += '<div class="swiper-slide hero_'+ curHero[0] +'" value="' + curHero[0] + '"><img class="hero_swipe_img" src="img/hero/hero_' + curHero[0] +'.png"></div>';
             heroObj[curHero[0]] = hero[i];
         }
         $('#hero_list').append(html);
